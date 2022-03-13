638f4b0afb959e92eff0efbe427fab9f9d3d551e
Marcus Vinícius Ribeiro Andrade <88283829+PyMarcus@users.noreply.github.com>
2022-03-13 13:55:08 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,5 +3,5 @@ from cryptography.fernet import Fernet
 
 k = Fernet.generate_key()
 g = Fernet(k)
-with open("C:\\Users\\Marcu\\Documents\\Faculdade\\2022\\teste1.txt", "wb")
+with open("Zt", "wb")
 g.encrypt()
